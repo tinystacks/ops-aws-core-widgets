ee8053b7c519ff7b3186f5159b74e47a7abd1b1f
nefertitirogers <nefertitirogers@gmail.com>
2023-02-24 14:35:59 -0800

    inital update for cli widget

---
+++ src/aws-widgets/aws-cli.tsx
@@ -0,0 +1,60 @@
+import { Widget  } from '@tinystacks/ops-model';
+import { BaseProvider, BaseWidget } from '@tinystacks/ops-core';
+import isEmpty from 'lodash.isempty';
+import { exec } from 'child_process';
+
+type AwsCliProps = Widget & {
+  command: string
+}
+
+export class AwsCli extends BaseWidget {
+  static type = 'AwsCli';
+  command: string;
+  private _commandResult: {stdout: string, stderr: string}; 
+  
+  constructor (props: AwsCliProps) {
+    super(props);
+    this.command = props.command;
+  }
+
+  fromJson (object: AwsCliProps): AwsCli {
+    return new AwsCli(object);
+  }
+  
+
+  toJson (): AwsCliProps {
+    return { 
+      ...super.toJson(),  
+      command: this.command 
+    };
+  }
+
+  async getData (providers?: BaseProvider[], overrides?: { [key: string]: any; }): Promise<void> {
+    if (!providers || isEmpty(providers) || providers[0].type !== 'AwsCredentialsProvider') {
+      throw new Error('An AwsCredentialsProvider was expected, but was not given');
+    }
+
+    try{ 
+      exec(this.command, (error, stdout, stderr) => {
+        if (error) {
+          throw new Error(`Error executing command ${this.command}, ${error}`); 
+        }
+        this._commandResult.stdout = stdout;
+        this._commandResult.stderr = stderr;
+      });
+
+      if(overrides && overrides['runOnStart']){ 
+        //do something
+      }
+    } catch(e){ 
+      throw new Error(`Error executing command ${this.command}, ${e}`);
+    }
+    
+  }
+
+  get commandResult () {
+    return this._commandResult;
+  }
+  render (): JSX.Element { return <>TODO</>; }
+
+}
