42177f22fdb4ee7581f41589fdb81333d92c3cdc
zsimjee <zaydsimjee@gmail.com>
2023-02-28 01:21:17 -0800

    Render JSON key/value

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from './aws-provider/aws-credentials-provider.js';
 export * from './aws-widgets/aws-cloud-watch-metric-graph.js';
 export * from './aws-widgets/aws-cloud-watch-logs.js';
+export * from './aws-widgets/json-tree.js';
