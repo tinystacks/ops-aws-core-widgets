aa6b7bacd62a48ff585368d39e38bebf5bd955c6
nefertitirogers <nefertitirogers@gmail.com>
2023-03-09 12:18:22 -0800

    update

--- src/aws-widgets/aws-cli.tsx
+++ src/aws-widgets/aws-cli.tsx
@@ -1,31 +1,37 @@
+import React from 'react';
 import { Widget  } from '@tinystacks/ops-model';
 import { BaseProvider, BaseWidget } from '@tinystacks/ops-core';
-import { exec } from 'child_process';
-import util from 'node:util';
+import { Box, Button, Heading, Stack } from '@chakra-ui/react';
 
-const execPromise = util.promisify(exec);
 
 type AwsCliProps = Widget & {
   command: string
+  runOnStart?: boolean
+}
+
+type AwsCliOverrides = { 
+  clear?: boolean, 
+  run?: boolean
+
 }
 
 export class AwsCli extends BaseWidget {
   static type = 'AwsCli';
   command: string;
+  runOnStart: boolean;
   private _commandResult: {stdout: string, stderr: string}; 
-  private _hasDataBeenFetched: boolean;
   
   constructor (props: AwsCliProps) {
     super(props);
     this.command = props.command;
+    this.runOnStart = props.runOnStart || true;
     this._commandResult = {
       stdout: '', 
       stderr: ''
     };
-    this._hasDataBeenFetched = false;
   }
 
-  fromJson (object: AwsCliProps): AwsCli {
+  static fromJson (object: AwsCliProps): AwsCli {
     return new AwsCli(object);
   }
   
@@ -33,33 +39,42 @@ export class AwsCli extends BaseWidget {
   toJson (): AwsCliProps {
     return { 
       ...super.toJson(),  
-      command: this.command 
+      command: this.command, 
+      runOnStart: this.runOnStart 
     };
   }
 
-  async getData (providers?: BaseProvider[], overrides?: { [key: string]: any; }): Promise<void> {
-    this._hasDataBeenFetched = true;
+  async getData (providers?: BaseProvider[], overrides?: AwsCliOverrides): Promise<void> {
+    const depMap = {
+      childproc: 'child_process',
+      nodeutil: 'node:util'
+    };
+    
+    const exec = (await import (depMap['childproc']))['exec'];
+    const promisify = (await import (depMap['nodeutil']))['promisify'];
+    const execPromise = promisify(exec); 
+
     try{ 
-      if(overrides && (overrides['runOnStart'] || overrides['run'])){ 
+      if(this.runOnStart){ 
         const { stdout, stderr } = await execPromise(this.command);
-        this._commandResult = { 
+        this.commandResult = { 
+          stdout: stdout,  
+          stderr: stderr
+        };
+        return;
+      }
+      if(overrides &&  overrides['run']){ 
+        const { stdout, stderr } = await execPromise(this.command);
+        this.commandResult = { 
           stdout: stdout,  
           stderr: stderr
         };
         return;
       }
       if(overrides && overrides['clear']){ 
-        this._commandResult = {
+        this.commandResult = {
           stdout: '', 
           stderr: ''
-        };
-        return;
-      }
-      if(!overrides){ 
-        const { stdout, stderr } = await execPromise(this.command);
-        this._commandResult = { 
-          stdout: stdout,  
-          stderr: stderr
         };
         return;
       }
@@ -73,10 +88,92 @@ export class AwsCli extends BaseWidget {
     return this._commandResult;
   }
 
-  get hasDataBeenFetched (){ 
-    return this._hasDataBeenFetched; 
+  set commandResult (_commandResult) { 
+    this._commandResult = _commandResult;
   }
   
-  render (): JSX.Element { return <>TODO</>; }
+  render (_children?: any, overridesCallback?: (overrides: AwsCliOverrides) => void): JSX.Element { 
+    const boxStyles = { 
+      overflow: 'scroll',
+      flex: 'none',
+      backgroundColor: '#EDF2F7',
+      color: '#000000',
+      height: '88px', 
+      margin: '14px',
+      padding: '24px', 
+      width: 'full',
+      alignSelf: 'stretch', 
+      fontFamily: 'monospace', 
+      fontStyle: 'normal',
+      fontSize: '14px',
+      fontWeight: '400',
+      lineHeight: '21px', 
+      borderRadius: '8px'  
+    };
+    const commandResultRender = (this._commandResult.stderr || this._commandResult.stdout) ? 
+      <Box style={boxStyles}>
+        {this.commandResult.stderr}
+        {this.commandResult.stdout}
+        <Button style={{
+          display: 'flex',
+          backgroundColor: '#FFFFFF', 
+          flex: 'none',
+          color: '#344054', 
+          fontFamily: 'Inter', 
+          fontStyle: 'normal',
+          fontWeight: '600', 
+          fontSize: '14px',
+          lineHeight: '20px', 
+          border: '1px solid #D0D5DD', 
+          borderRadius: '8px', 
+          float: 'right'
+        }} onClick={
+          () => overridesCallback({ clear: true })} >
+          Clear
+        </Button>
+      </Box> : 
+      <Box style={boxStyles}>
+          Command has not been run yet!
+      </Box>;
+
+    return (
+      <Stack style={{ backgroundColor: '#ffffff', width: '100%' }}>
+        <Box 
+          style={boxStyles}
+        >
+          {this.command}
+          <Button  style={{
+            display: 'flex',
+            backgroundColor: '#7F56D9', 
+            flex: 'none',
+            color: '#FFFFFF', 
+            fontFamily: 'Inter', 
+            fontStyle: 'normal',
+            fontWeight: '600', 
+            fontSize: '14px',
+            lineHeight: '20px', 
+            border: '1px solid #7F56D9', 
+            borderRadius: '8px', 
+            float: 'right'
+          }} onClick={
+            () => overridesCallback({ run: true })}>
+          Run
+          </Button>
+        </Box>
+        <Heading style={{ 
+          color: '#000000', 
+          fontFamily: 'Inter', 
+          fontStyle: 'normal',
+          fontWeight: '600', 
+          fontSize: '16px',
+          lineHeight: '24px', 
+          paddingLeft: '24px'
+        }}> 
+          Response:
+        </Heading>
+        { commandResultRender }
+      </Stack>
+    );
+  }
 
 }
