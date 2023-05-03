d43505c70a963573db5403e6bc4a6acde9b20823
nefertitirogers <nefertitirogers@gmail.com>
2023-05-03 14:23:09 -0700

    update render functions

--- test/widgets/aws-cloud-watch-logs.test.tsx
+++ test/widgets/aws-cloud-watch-logs.test.tsx
@@ -1,6 +1,7 @@
-import { cleanup } from '@testing-library/react';
+import { cleanup, render, screen } from '@testing-library/react';
 import { AwsCloudWatchLogs } from '../../src/aws-widgets/aws-cloud-watch-logs.js'
 import { TimeUnitEnum } from '../../src/utils/utils';
+import '@testing-library/jest-dom/extend-expect';
 
 describe('AwsCloudWatchLogs', () => {
   afterEach(cleanup);
@@ -79,6 +80,63 @@ describe('AwsCloudWatchLogs getData function', () => {
   it('should throw an error if the first provider is not an AwsCredentialsProvider', async () => {
     mockProviders[0].type = 'NotAwsCredentialsProvider';
     await expect(widget.getData(mockProviders, mockOverrides)).rejects.toThrow('An AwsCredentialsProvider was expected, but was not given');
+  });
+
+});
+
+describe('AwsCloudWatchLogs render', () => {
+  test('renders events correctly', () => {
+
+    const props = {
+      id: 'MockWidget',
+      type: 'AwsCloudWatchLogs',
+      displayName: 'mock cw widget', 
+      region: 'us-east-1', 
+      timeRange: { 
+        startTime: (Date.now() - ((7 * 24 * 60 * 60 * 1000))) , 
+        endTime: Date.now()
+      },
+      logGroupName: 'groupName', 
+      events: [
+      {
+        timestamp: 1683147148723,
+        message: 'Event 1',
+      },
+      {
+        timestamp: 1683060748723,
+        message: 'Event 2',
+      }]
+    };
+
+    const cwWidget = AwsCloudWatchLogs.fromJson(props);
+    const renderedWidget = cwWidget.render();
+    render(renderedWidget);
+
+      // Check that the events are rendered correctly
+    expect(screen.getByText('Event 1')).toBeInTheDocument();
+    expect(screen.getByText('Event 2')).toBeInTheDocument();
+  });
+
+  test('renders "no logs" message when events list is empty', () => {
+    const props = {
+      id: 'MockWidget',
+      type: 'AwsCloudWatchLogs',
+      displayName: 'mock cw widget', 
+      region: 'us-east-1', 
+      timeRange: { 
+        startTime: (Date.now() - ((7 * 24 * 60 * 60 * 1000))) , 
+        endTime: Date.now()
+      },
+      logGroupName: 'groupName', 
+      events: []
+    };
+
+    const cwWidget = AwsCloudWatchLogs.fromJson(props);
+    const renderedWidget = cwWidget.render();
+    render(renderedWidget);
+
+    // Check that the "no logs" message is rendered
+    expect(screen.getByText('There are no logs in this logGroup and logStream during this timeframe')).toBeInTheDocument();
   });
 
 });
--- test/widgets/aws-ecs-deployments.test.tsx
+++ test/widgets/aws-ecs-deployments.test.tsx
@@ -1,5 +1,6 @@
-import { cleanup } from '@testing-library/react';
+import { render, screen, cleanup } from '@testing-library/react';
 import { AwsEcsDeployments } from '../../src/aws-widgets/aws-ecs-deployments.js'
+import '@testing-library/jest-dom/extend-expect';
 
 describe('AwsEcsDeployments', () => {
   afterEach(cleanup);
@@ -74,3 +75,80 @@ describe('AwsEcsDeployments getData function', () => {
   });
 
 });
+
+
+describe('AwsEcsDeployments render', () => {
+  const deployments = [
+    {
+      deploymentId: '123',
+      status: 'RUNNING',
+      startedAt: new Date('2022-01-01T00:00:00Z'),
+      desiredCount: 2,
+      runningCount: 1,
+      pendingCount: 0,
+      taskDefinition: {
+        tasks: [
+          {
+            taskId: 'task-1',
+            startTime: new Date('2022-01-01T00:00:00Z'),
+            stopTime: null,
+            status: 'RUNNING'
+          },
+          {
+            taskId: 'task-2',
+            startTime: new Date('2022-01-01T00:00:00Z'),
+            stopTime: new Date('2022-01-01T00:01:00Z'),
+            status: 'STOPPED'
+          }
+        ]
+      }
+    }
+  ];
+
+  it('renders deployments table', () => {
+    const props = {
+      id: 'MockWidget',
+      type: 'AwsEcsDeployments',
+      displayName: 'mock widget', 
+      region: 'us-east-1', 
+      clusterName: 'cluster-name',
+      serviceName: 'service-name', 
+      deployments
+    };
+
+    const ecsWidget = AwsEcsDeployments.fromJson(props);
+    const renderedWidget = ecsWidget.render();
+    render(renderedWidget);
+    const deploymentId = screen.getByText('Deployment Id');
+    const deploymentStatus = screen.getByText('Deployment Status');
+    const started = screen.getAllByText('Started')[0];
+    const runningPendingDesired = screen.getByText('Running/Pending/Desired');
+    const killTaskBtn = screen.getAllByText('Kill task')[0];
+    expect(deploymentId).toBeInTheDocument();
+    expect(deploymentStatus).toBeInTheDocument();
+    expect(started).toBeInTheDocument();
+    expect(runningPendingDesired).toBeInTheDocument();
+    expect(killTaskBtn).toBeInTheDocument();
+  });
+
+  it('calls overridesCallback when kill task button is clicked', () => {
+    const mockOverridesCallback = jest.fn();
+    const props = {
+      id: 'MockWidget',
+      type: 'AwsEcsDeployments',
+      displayName: 'mock widget', 
+      region: 'us-east-1', 
+      clusterName: 'cluster-name',
+      serviceName: 'service-name', 
+      deployments
+    };
+
+    const ecsWidget = AwsEcsDeployments.fromJson(props);
+    const renderedWidget = ecsWidget.render(undefined, mockOverridesCallback);
+    render(renderedWidget);
+    const killTaskBtn = screen.getAllByText('Kill task')[0];
+    killTaskBtn.click();
+    expect(mockOverridesCallback).toHaveBeenCalledTimes(1);
+  });
+});
+
